edb57b39f61484ca4112194937548affc2cb9286
ekinsey <eairrick@gmail.com>
2025-01-17 13:57:26 -0500

    Add files via upload
    Init load

---
+++ readme.txt
@@ -0,0 +1,4 @@
+Paycode = workforce Manager (time, mileage, oncall)
+Pay Data = Lyric (per diem rates and productivity incentive)
+
+EPIPQE659 = export file from HCHB
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.fileflex.fileflex {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.fileflex.fileflex to javafx.fxml;
+    exports com.fileflex.fileflex;
+}
